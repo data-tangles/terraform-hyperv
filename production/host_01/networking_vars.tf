bd896836a828c6358bf2f7463270892d447d13cf
Cuan Leo <cuanleo@gmail.com>
2023-07-24 11:17:40 +0100

    separate networking per host

--- production/host_01/networking_vars.tf
+++ production/host_01/networking_vars.tf
@@ -1,16 +1,16 @@
 # Production
 
-variable "production_vswitch_name" {
+variable "host_01_production_vswitch_name" {
   type        = string
   description = "Specifies the name of the switch to be created"
 }
 
-variable "production_vswitch_allow_management_os" {
+variable "host_01_production_vswitch_allow_management_os" {
   type        = bool
   description = "Specifies if the HyperV host machine will have access to network switch when created"
 }
 
-variable "production_vswitch_switch_type" {
+variable "host_01_production_vswitch_switch_type" {
   type        = string
   description = "Specifies the type of the switch to be created"
 }
--- production/host_02/networking.tf
+++ production/host_02/networking.tf
@@ -1,8 +1,8 @@
 # Production vSwitch
 
-resource "hyperv_network_switch" "production_vswitch" {
-  name                = var.production_vswitch_name
+resource "hyperv_network_switch" "host_02_production_vswitch" {
+  name                = var.host_02_production_vswitch_name
   notes               = "Environment: ${local.environment}\nCreated by: ${local.createdby}\nCreated on: ${local.createdon}\nModified on: ${local.modifiedon}\nPurpose: Production vSwitch"
-  allow_management_os = var.production_vswitch_allow_management_os
-  switch_type         = var.production_vswitch_switch_type
+  allow_management_os = var.host_02_production_vswitch_allow_management_os
+  switch_type         = var.host_02_production_vswitch_switch_type
 }
