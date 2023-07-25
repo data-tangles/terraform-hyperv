1ab3d3599fa974c4a94fa57993f9afbe4b4295b5
Cuan Leo <cuanleo@gmail.com>
2023-07-25 10:23:20 +0100

    networking changes

--- production/host_02/networking.tf
+++ production/host_02/networking.tf
@@ -1,8 +1,15 @@
 # Production vSwitch
 
 resource "hyperv_network_switch" "host_02_production_vswitch" {
-  name                = var.host_02_production_vswitch_name
-  notes               = "Environment: ${local.environment}\nCreated by: ${local.createdby}\nModified on: ${local.modifiedon}\nPurpose: Production vSwitch"
-  allow_management_os = var.host_02_production_vswitch_allow_management_os
-  switch_type         = var.host_02_production_vswitch_switch_type
+  name                                    = var.host_02_production_vswitch_name
+  notes                                   = "Environment: ${local.environment}\nCreated by: ${local.createdby}\nModified on: ${local.modifiedon}\nPurpose: Production vSwitch"
+  allow_management_os                     = var.host_02_production_vswitch_allow_management_os
+  switch_type                             = var.host_02_production_vswitch_switch_type
+  default_flow_minimum_bandwidth_absolute = "100000000"
+  default_queue_vmmq_enabled              = "true"
+  default_queue_vrss_enabled              = "true"
+  minimum_bandwidth_mode                  = "Absolute"
+  net_adapter_names = [
+    "Ethernet"
+  ]
 }
